d2010716c769318171d7fc6572d505866c581852
Your Name <your_github_email@example.com>
2025-11-02 23:01:06 -0500

    feat: initial node app

---
+++ index.js
@@ -0,0 +1,6 @@
+﻿const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (_req, res) => res.send("Hello from Jenkins CI!"));
+app.listen(port, () => console.log(`Server up on :${port}`));
